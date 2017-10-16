8c420dafb3290eeaf6e18b66039aad82fc1f3328
Shia <rise.shia@gmail.com>
2017-10-16 19:55:23 +0900

    Add SyncChannelsJob

---
+++ app/jobs/sync_channels_job.rb
@@ -0,0 +1,44 @@
+class SyncChannelsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    channels.each do |c|
+      if c.is_archived
+        Rails.logger.info "#{c.name} is skiped because it archived"
+        next
+      end
+
+      ch = channel(c.id)
+      unless ch
+        Rails.logger.info "Bot can't trace #{c.name}. Please invite him"
+        next
+      end
+
+      obj = Channel.find_or_create_by(cid: ch.id) do |obj|
+        obj.name = ch.name
+        obj.master = "Nobody"
+      end
+      obj.last_updated_at = Time.zone.at(ch.latest.ts.to_i)
+      obj.save
+    end
+  end
+
+  private
+
+  def client
+    @client ||= Slack::Web::Client.new
+  end
+
+  def channels
+    @channels ||= client.channels_list.channels
+  end
+
+  def channel(cid)
+    ch = client.channels_info(channel: cid).channel
+    if ch.is_member
+      ch
+    else
+      false
+    end
+  end
+end
---
+++ test/jobs/sync_channels_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SyncChannelsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
