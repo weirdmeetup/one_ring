e8df7e47d892a7027be0f267b2266e1257c5268f
Shia <rise.shia@gmail.com>
2017-10-18 22:08:41 +0900

    Remove json

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -26,28 +26,20 @@ class ChannelsController < ApplicationController
   def create
     @channel = Channel.new(channel_params)
 
-    respond_to do |format|
-      if @channel.save
-        format.html { redirect_to @channel, notice: 'Channel was successfully created.' }
-        format.json { render :show, status: :created, location: @channel }
-      else
-        format.html { render :new }
-        format.json { render json: @channel.errors, status: :unprocessable_entity }
-      end
+    if @channel.save
+      redirect_to @channel, notice: 'Channel was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /channels/1
   # PATCH/PUT /channels/1.json
   def update
-    respond_to do |format|
-      if @channel.update(channel_params)
-        format.html { redirect_to @channel, notice: 'Channel was successfully updated.' }
-        format.json { render :show, status: :ok, location: @channel }
-      else
-        format.html { render :edit }
-        format.json { render json: @channel.errors, status: :unprocessable_entity }
-      end
+    if @channel.update(channel_params)
+      redirect_to @channel, notice: 'Channel was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -55,20 +47,17 @@ class ChannelsController < ApplicationController
   # DELETE /channels/1.json
   def destroy
     @channel.archive
-    respond_to do |format|
-      format.html { redirect_to channels_url, notice: 'Channel was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to channels_url, notice: 'Channel was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_channel
-      @channel = Channel.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_channel
+    @channel = Channel.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def channel_params
-      params.require(:channel).permit(:cid, :name, :master)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def channel_params
+    params.require(:channel).permit(:cid, :name, :master)
+  end
 end
