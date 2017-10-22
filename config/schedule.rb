6231e42fc27f46a459bb6db6bb29b9b26be64ee8
Shia <rise.shia@gmail.com>
2017-10-22 19:54:47 +0900

    Remove Schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 # Learn more: http://github.com/javan/whenever
 
-every 1.minute, at: '6:30 am' do
-  runner 'WarningJob.perform_now'
-end
+# every 1.minute, at: '6:30 am' do
+#   runner 'WarningJob.perform_now'
+# end
 
 # every 1.day, at: '6:40 am' do
 #   runner "AchivingJob.perform_now"
