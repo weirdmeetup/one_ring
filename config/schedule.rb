f9486031aedbbbb59a9a1a32ed292e9cfd3a2f5d
Shia <rise.shia@gmail.com>
2017-11-09 20:33:14 +0900

    Add cron for SyncChannelJob

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,10 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 env :PATH, ENV["PATH"]
 set :output, "log/cron.log"
 
+every 1.day, at: "09:20 pm" do
+  runner "SyncChannelsJob.perform_now"
+end
+
 every 1.day, at: "09:30 pm" do
   runner "WarningJob.perform_now"
 end
